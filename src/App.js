8ed16e5c6fea1f4d830624aa77ef009c51cf38db
Gabriel Sleenhof <gsleenhof@MacBook-Air-6.local>
2021-12-30 17:28:19 -0500

    Adding the placeholder to the inputs

--- src/App.js
+++ src/App.js
@@ -47,9 +47,11 @@ useEffect(() => {
         </h1>
 			<h2>Your Stakes are {stakes} </h2>
 			{/* <button onClick={() => fetchHexBalance()}>Refresh balance</button> */}
-      <h3>Shares to stake (Your Hex balance is {balance} HEX) <br/>Shares: <input type="text" id="myText"/></h3>
-    <p> Description: How long the recipient wants to stake HEX for <br/> Time: <input type="text" id="myText"/></p>
-    <p> Description: What percentage of the T-Shares the recipient will keep as a premium<br/> Premium: <input type="text" id="myText"/> </p>
+      <h3>Shares to stake (Your Hex balance is {balance} HEX) <br/>Shares: <input autocomplete="off" type="text" id="myText" placeholder="Shares to Stake"/></h3>
+	<p> Description: The contract to receive the newly minter shares <br/> Receiver: <input autocomplete="off" type="text" id="myText" placeholder="Receiver Address"/></p>
+	<p> Description: The reimbursement address for the supplies <br/> Supplier: <input autocomplete="off" type="text" id="myText" placeholder="Supplier Address"/></p>
+	<p> Description: How long the recipient wants to stake HEX for <br/> Time: <input autocomplete="off" type="text" id="myText" placeholder="Length of Stake (in Days)"/></p>
+    <p> Description: What percentage of the T-Shares the recipient will keep as a premium<br/> Premium: <input autocomplete="off" type="text" id="myText" placeholder="0-99.9%"/> </p>
     <button type="button">Stake</button>
     <p id="demo"></p>
 		</div>
